1dcd8d1eedda7dbde868b6285f63c58f1b5be211
rondripepic <rondrip112507@gmail.com>
2026-05-23 00:07:02 +0000

    first commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int rng = random.nextInt(100);
+        System.out.println("Random number: " + rng);
+        System.out.print("Enter your number: ");
+        int usernumber = scanner.nextInt();
+        while(usernumber != rng){
+            System.out.println("Please enter a number between 0 and 100.");
+            System.out.print("Enter your number: ");
+            usernumber = scanner.nextInt();
+         if(usernumber == rng){
+            System.out.println("You guessed it!");
+            scanner.close();
+        } else {
+            System.out.println("You didn't guess it!");
+        }
+        }
+
+    }
+}
